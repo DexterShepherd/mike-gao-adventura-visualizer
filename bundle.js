6e7fb38dfa65146754c93108fcff5b70134f29d0
Dexter Shepherd <dexter@kadenze.com>
2017-03-04 10:42:51 -0800

    added play button

--- bundle.js
+++ bundle.js
@@ -54,236 +54,243 @@
 
 	'use strict';
 
-	var THREE = __webpack_require__(2)(['OBJLoader']);
-	var p5 = __webpack_require__(67);
-	var sound = __webpack_require__(68);
-	var dom = __webpack_require__(69);
-	var NOC = __webpack_require__(70);
+	document.addEventListener("DOMContentLoaded", function (event) {
+	  var playButton = document.querySelector('#play');
+	  playButton.addEventListener('click', function () {
+	    var THREE = __webpack_require__(2)(['OBJLoader']);
+	    var p5 = __webpack_require__(67);
+	    var sound = __webpack_require__(68);
+	    var dom = __webpack_require__(69);
+	    var NOC = __webpack_require__(70);
 
-	var Fade = __webpack_require__(71);
+	    var Fade = __webpack_require__(71);
 
-	new p5(function (p) {
-	  var input = void 0,
-	      drumFFT = void 0,
-	      drum = void 0,
-	      lp = void 0,
-	      playback = void 0,
-	      mute = void 0;
+	    Fade.out(playButton);
 
-	  p.preload = function () {
-	    // playback = p.loadSound('data/mike-gao.mp3');
-	    // drum = p.loadSound('data/mike-gao.mp3');
+	    new p5(function (p) {
+	      var input = void 0,
+	          drumFFT = void 0,
+	          drum = void 0,
+	          lp = void 0,
+	          playback = void 0,
+	          mute = void 0;
 
-	    var audioPath = 'humbled.mp3';
-	    drum = p.loadSound('data/mike-gao-drum.mp3');
-	    playback = p.loadSound('data/mike-gao.mp3');
-	  };
+	      p.preload = function () {
+	        // playback = p.loadSound('data/mike-gao.mp3');
+	        // drum = p.loadSound('data/mike-gao.mp3');
 
-	  p.setup = function () {
-	    // move default canvas out of the way
-	    var c = p.createCanvas(0, 0);
-	    c.position(-9999, -9999);
-	    setupAudio();
-	    init();
-	    animate();
+	        var audioPath = 'humbled.mp3';
+	        drum = p.loadSound('data/mike-gao-drum.mp3');
+	        playback = p.loadSound('data/mike-gao.mp3');
+	      };
 
-	    playback.onended(function () {
-	      ended = true;
+	      p.setup = function () {
+	        // move default canvas out of the way
+	        var c = p.createCanvas(0, 0);
+	        c.position(-9999, -9999);
+	        setupAudio();
+	        init();
+	        animate();
+
+	        playback.onended(function () {
+	          ended = true;
+	        });
+	      };
+
+	      p.draw = function () {
+	        drumFFT.analyze();
+	        var en = drumFFT.getEnergy(440);
+	        audioTrigger = en;
+	      };
+
+	      function setupAudio() {
+	        drumFFT = new p5.FFT();
+	        drum.disconnect();
+	        lp = new p5.LowPass();
+	        //440
+	        lp.freq(440);
+	        lp.disconnect();
+	        drum.connect(lp);
+	        drumFFT.setInput(lp);
+	        playback.play();
+	        drum.play();
+	        mute = new p5.Gain();
+	        drum.connect(mute);
+	        mute.amp(0);
+	      }
 	    });
-	  };
 
-	  p.draw = function () {
-	    drumFFT.analyze();
-	    var en = drumFFT.getEnergy(440);
-	    audioTrigger = en;
-	  };
+	    var container = void 0,
+	        clock = new THREE.Clock(true),
+	        clockBig = new THREE.Clock(true);
+	    var camera = void 0,
+	        scene = void 0,
+	        renderer = void 0,
+	        particles = void 0,
+	        geometry = void 0,
+	        material = void 0,
+	        parameters = void 0,
+	        i = void 0,
+	        h = void 0,
+	        color = void 0,
+	        size = void 0;
+	    var dests = void 0,
+	        sphere = void 0,
+	        testers = [],
+	        speed = 0,
+	        num_points = Math.round(Math.random() * 10);
+	    var movers = void 0;
+	    var mouseX = 0,
+	        mouseY = 0;
+	    var audioTrigger = 0;
+	    var composer = void 0;
+	    var timeSinceLastTrigger = 0;
+	    var windowHalfX = window.innerWidth / 2;
+	    var windowHalfY = window.innerHeight / 2;
 
-	  function setupAudio() {
-	    drumFFT = new p5.FFT();
-	    drum.disconnect();
-	    lp = new p5.LowPass();
-	    //440
-	    lp.freq(440);
-	    lp.disconnect();
-	    drum.connect(lp);
-	    drumFFT.setInput(lp);
-	    playback.play();
-	    drum.play();
-	    mute = new p5.Gain();
-	    drum.connect(mute);
-	    mute.amp(0);
-	  }
-	});
+	    var text = null;
+	    var loaded = false;
 
-	var container = void 0,
-	    clock = new THREE.Clock(true),
-	    clockBig = new THREE.Clock(true);
-	var camera = void 0,
-	    scene = void 0,
-	    renderer = void 0,
-	    particles = void 0,
-	    geometry = void 0,
-	    material = void 0,
-	    parameters = void 0,
-	    i = void 0,
-	    h = void 0,
-	    color = void 0,
-	    size = void 0;
-	var dests = void 0,
-	    sphere = void 0,
-	    testers = [],
-	    speed = 0,
-	    num_points = Math.round(Math.random() * 10);
-	var movers = void 0;
-	var mouseX = 0,
-	    mouseY = 0;
-	var audioTrigger = 0;
-	var composer = void 0;
-	var timeSinceLastTrigger = 0;
-	var windowHalfX = window.innerWidth / 2;
-	var windowHalfY = window.innerHeight / 2;
+	    var exploded = false;
+	    var trigger = false;
+	    var triggerBig = false;
 
-	var text = null;
-	var loaded = false;
+	    var ended = false;
 
-	var exploded = false;
-	var trigger = false;
-	var triggerBig = false;
+	    function init() {
+	      container = document.createElement('div');
+	      container.style.position = 'absolute';
+	      container.style.top = '0';
+	      container.style.left = '0';
+	      container.style.zIndex = '-1';
+	      document.body.appendChild(container);
 
-	var ended = false;
+	      camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 3000);
+	      camera.position.z = 1000;
+	      scene = new THREE.Scene();
+	      geometry = new THREE.Geometry();
+	      movers = [];
 
-	function init() {
-	  container = document.createElement('div');
-	  container.style.position = 'absolute';
-	  container.style.top = '0';
-	  container.style.left = '0';
-	  container.style.zIndex = '-1';
-	  document.body.appendChild(container);
+	      for (i = 0; i < 60000; i++) {
+	        var vertex = new THREE.Vector3(_random(window.innerWidth), _random(window.innerHeight), _random(1000));
+	        geometry.vertices.push(vertex);
+	        movers.push(new NOC.Particle(vertex, i % num_points));
+	      }
 
-	  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 3000);
-	  camera.position.z = 1000;
-	  scene = new THREE.Scene();
-	  geometry = new THREE.Geometry();
-	  movers = [];
+	      material = new THREE.PointsMaterial({ size: 2, color: 0xffffff });
+	      particles = new THREE.Points(geometry, material);
 
-	  for (i = 0; i < 60000; i++) {
-	    var vertex = new THREE.Vector3(_random(window.innerWidth), _random(window.innerHeight), _random(1000));
-	    geometry.vertices.push(vertex);
-	    movers.push(new NOC.Particle(vertex, i % num_points));
-	  }
+	      scene.add(particles);
 
-	  material = new THREE.PointsMaterial({ size: 2, color: 0xffffff });
-	  particles = new THREE.Points(geometry, material);
+	      renderer = new THREE.WebGLRenderer();
+	      renderer.setPixelRatio(window.devicePixelRatio);
+	      renderer.setSize(window.innerWidth, window.innerHeight);
 
-	  scene.add(particles);
+	      container.appendChild(renderer.domElement);
+	      document.addEventListener('mousemove', onDocumentMouseMove, false);
+	      document.addEventListener('touchstart', onDocumentTouchStart, false);
+	      document.addEventListener('touchmove', onDocumentTouchMove, false);
+	      window.addEventListener('resize', onWindowResize, false);
 
-	  renderer = new THREE.WebGLRenderer();
-	  renderer.setPixelRatio(window.devicePixelRatio);
-	  renderer.setSize(window.innerWidth, window.innerHeight);
+	      var hero = document.querySelector('#hero');
+	      Fade.in(hero, 10000);
 
-	  container.appendChild(renderer.domElement);
-	  document.addEventListener('mousemove', onDocumentMouseMove, false);
-	  document.addEventListener('touchstart', onDocumentTouchStart, false);
-	  document.addEventListener('touchmove', onDocumentTouchMove, false);
-	  window.addEventListener('resize', onWindowResize, false);
-
-	  var hero = document.querySelector('#hero');
-	  Fade.in(hero, 10000);
-
-	  setTimeout(function () {
-	    Fade.out(hero, 10000);
-	  }, 5000);
-	}
-
-	function restart() {
-	  location.reload();
-	}
-
-	function animate() {
-	  if (ended) {
-	    return;
-	  }
-
-	  speed += mapRange(audioTrigger, 0, 255, 0.001, 0.020);
-
-	  trigger = audioTrigger > 200 && clock.getDelta() > 0.05;
-	  triggerBig = audioTrigger > 210 && clockBig.getDelta() > 0.05;
-
-	  particles.geometry.vertices.forEach(function (v, index) {
-
-	    if (exploded) {
-	      movers[index].scaler = 0.01;
-	      movers[index].vel.copy(new THREE.Vector3(0, 0, 0));
-	      exploded = false;
+	      setTimeout(function () {
+	        Fade.out(hero, 10000);
+	      }, 5000);
 	    }
 
-	    if (trigger) {
-	      movers[index].vel.copy(new THREE.Vector3(_random(10), _random(10), _random(10)));
-	      num_points = Math.floor(Math.random() * 20) + 1;
-	      movers[index].scaler = 1 - movers[index].scaler;
-	      if (triggerBig) {
-	        movers[index].scaler = Math.random() * 3;
-	        exploded = true;
+	    function restart() {
+	      location.reload();
+	    }
+
+	    function animate() {
+	      if (ended) {
+	        return;
+	      }
+
+	      speed += mapRange(audioTrigger, 0, 255, 0.001, 0.020);
+
+	      trigger = audioTrigger > 200 && clock.getDelta() > 0.05;
+	      triggerBig = audioTrigger > 210 && clockBig.getDelta() > 0.05;
+
+	      particles.geometry.vertices.forEach(function (v, index) {
+
+	        if (exploded) {
+	          movers[index].scaler = 0.01;
+	          movers[index].vel.copy(new THREE.Vector3(0, 0, 0));
+	          exploded = false;
+	        }
+
+	        if (trigger) {
+	          movers[index].vel.copy(new THREE.Vector3(_random(10), _random(10), _random(10)));
+	          num_points = Math.floor(Math.random() * 20) + 1;
+	          movers[index].scaler = 1 - movers[index].scaler;
+	          if (triggerBig) {
+	            movers[index].scaler = Math.random() * 3;
+	            exploded = true;
+	          }
+	        }
+	        v.copy(movers[index].update(v, num_points, speed));
+	      });
+
+	      particles.rotation.z = speed * 0.01;
+	      particles.geometry.verticesNeedUpdate = true;
+
+	      requestAnimationFrame(animate);
+	      render();
+
+	      trigger = false;
+	      triggerBig = false;
+	    }
+
+	    function render() {
+	      var time = Date.now() * 0.00005;
+	      camera.position.x += (mouseX - camera.position.x) * 0.05;
+	      camera.position.y += (-mouseY - camera.position.y) * 0.05;
+	      camera.lookAt(scene.position);
+
+	      renderer.render(scene, camera);
+	    }
+
+	    function mapRange(value, oldMin, oldMax, newMin, newMax) {
+	      return (value - oldMin) * (newMax - newMin) / (oldMax - oldMin) + newMin;
+	    }
+
+	    function _random(size) {
+	      return Math.random() * size - size / 2.0;
+	    }
+
+	    function onWindowResize() {
+	      windowHalfX = window.innerWidth / 2;
+	      windowHalfY = window.innerHeight / 2;
+	      camera.aspect = window.innerWidth / window.innerHeight;
+	      camera.updateProjectionMatrix();
+	      renderer.setSize(window.innerWidth, window.innerHeight);
+	    }
+
+	    function onDocumentMouseMove(event) {
+	      mouseX = event.clientX - windowHalfX;
+	      mouseY = event.clientY - windowHalfY;
+	    }
+
+	    function onDocumentTouchStart(event) {
+	      if (event.touches.length === 1) {
+	        event.preventDefault();
+	        mouseX = event.touches[0].pageX - windowHalfX;
+	        mouseY = event.touches[0].pageY - windowHalfY;
 	      }
 	    }
-	    v.copy(movers[index].update(v, num_points, speed));
+
+	    function onDocumentTouchMove(event) {
+	      if (event.touches.length === 1) {
+	        event.preventDefault();
+	        mouseX = event.touches[0].pageX - windowHalfX;
+	        mouseY = event.touches[0].pageY - windowHalfY;
+	      }
+	    }
 	  });
-
-	  particles.rotation.z = speed * 0.01;
-	  particles.geometry.verticesNeedUpdate = true;
-
-	  requestAnimationFrame(animate);
-	  render();
-
-	  trigger = false;
-	  triggerBig = false;
-	}
-
-	function render() {
-	  var time = Date.now() * 0.00005;
-	  camera.position.x += (mouseX - camera.position.x) * 0.05;
-	  camera.position.y += (-mouseY - camera.position.y) * 0.05;
-	  camera.lookAt(scene.position);
-
-	  renderer.render(scene, camera);
-	}
-
-	function mapRange(value, oldMin, oldMax, newMin, newMax) {
-	  return (value - oldMin) * (newMax - newMin) / (oldMax - oldMin) + newMin;
-	}
-
-	function _random(size) {
-	  return Math.random() * size - size / 2.0;
-	}
-
-	function onWindowResize() {
-	  windowHalfX = window.innerWidth / 2;
-	  windowHalfY = window.innerHeight / 2;
-	  camera.aspect = window.innerWidth / window.innerHeight;
-	  camera.updateProjectionMatrix();
-	  renderer.setSize(window.innerWidth, window.innerHeight);
-	}
-
-	function onDocumentMouseMove(event) {
-	  mouseX = event.clientX - windowHalfX;
-	  mouseY = event.clientY - windowHalfY;
-	}
-
-	function onDocumentTouchStart(event) {
-	  if (event.touches.length === 1) {
-	    event.preventDefault();
-	    mouseX = event.touches[0].pageX - windowHalfX;
-	    mouseY = event.touches[0].pageY - windowHalfY;
-	  }
-	}
-
-	function onDocumentTouchMove(event) {
-	  if (event.touches.length === 1) {
-	    event.preventDefault();
-	    mouseX = event.touches[0].pageX - windowHalfX;
-	    mouseY = event.touches[0].pageY - windowHalfY;
-	  }
-	}
+	});
 
 /***/ },
 /* 2 */
